b073ff514d3ed9717464354cce61371b32dbdaee
nicholaseruplarsen <109358056+nicholaseruplarsen@users.noreply.github.com>
2024-01-03 22:17:35 +0100

    Added PDFs to /projects

---
+++ app/chessai/page.tsx
@@ -0,0 +1,8 @@
+export default function Chess() {
+  return (
+    <iframe 
+      src='/pdfs/02461.pdf' 
+      style={{ width: '100%', height: '100vh' }} // Set appropriate width and height
+    />
+  );
+}
---
+++ app/ux/page.tsx
@@ -0,0 +1,9 @@
+export default function UX() {
+    return (
+      <iframe 
+        src='/pdfs/02809.pdf' 
+        style={{ width: '100%', height: '100vh' }} // Set appropriate width and height
+      />
+    );
+  }
+  
