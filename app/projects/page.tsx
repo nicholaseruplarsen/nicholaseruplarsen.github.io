50dde83d44290af837820d402c011e4e806ae747
nicholaseruplarsen <nicholaserup@gmail.com>
2024-01-04 12:50:01 +0100

    Added project descriptions

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -27,19 +27,16 @@ export default function Projects() {
       <div className={styles.grid}>
         <a className={styles.card}  href="ux" target="_blank" rel="noopener noreferrer">
           <h2>UX Design Project <span>-&gt;</span></h2>
-          <br />
+          <h6>November 2023</h6>
           <Image unoptimized className={styles.cardImage} src={landingpage} alt="Landing Page" />
-          <br />
           <Image unoptimized className={styles.cardImage} src={iteration} alt="Iteration" />
-          <br /><br />
-          <p>November 2023. Designing, prototyping, and iterating app designs with the customer in focus. <br /><br /> Course was a lot better than I expected.</p>
+          <p>Designing, prototyping, and iterating executable app designs and landing pages with the customer in focus. <br/><br/> Became really experienced with Figma throughout this course.</p>
         </a>
         <a className={styles.card}  href="/chessai" target="_blank" rel="noopener noreferrer">
           <h2>Chess CNN <span>-&gt;</span></h2>
-          <br />
+          <h6>January 2023</h6>
           <Image unoptimized className={styles.cardImage} src={chessboard} alt="Chess board" />
-          <br /><br />
-          <p>January 2023. Trained a convolutional neural network (CNN) on large datasets of pro players&apos; moves to emulate their playing style and let stockfish play out the rest if the games were a draw. Used DTU&apos;s high performance computer (HPC) to train the model.</p>
+          <p>Trained a convolutional neural network (CNN) on large datasets of pro players&apos; moves to emulate their playing style and let stockfish play out the rest if the games were a draw. Used DTU&apos;s high performance computer (HPC) to train the model.</p>
         </a>
       </div>
     </main>
